6d3766cf0f98bdb20bde21be89aa82011c1138a2
huskyhania <h.skrzypiec@gmail.com>
2025-11-07 18:17:17 +0200

    cloudfront logic

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -7,6 +7,8 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigwv2 from "aws-cdk-lib/aws-apigatewayv2";
 import * as apigwInt from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 import { execSync } from "child_process";
 
@@ -16,9 +18,9 @@ export class InfraStack extends cdk.Stack {
 
     execSync("npm ci && npm run build", {
       cwd: path.join(__dirname, "../../frontend"),
-      stdio: "inherit"
+      stdio: "inherit",
     });
-    
+
     const siteBucket = new s3.Bucket(this, "FrontendBucket", {
       websiteIndexDocument: "index.html",
       publicReadAccess: true,
@@ -60,18 +62,43 @@ export class InfraStack extends cdk.Stack {
       },
     });
 
-    // --- Upload prebuilt frontend ---
+    // --- Deploy prebuilt frontend to S3 ---
     new s3deploy.BucketDeployment(this, "DeployFrontend", {
       sources: [s3deploy.Source.asset(path.join(__dirname, "../../frontend/dist"))],
       destinationBucket: siteBucket,
     });
 
+    // --- CloudFront distribution ---
+    const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      additionalBehaviors: {
+        "/api/*": {
+          origin: new origins.HttpOrigin(
+            // Extract domain from API Gateway URL (e.g., https://xxxx.execute-api.region.amazonaws.com)
+            cdk.Fn.select(2, cdk.Fn.split("/", api.apiEndpoint))
+          ),
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      },
+      defaultRootObject: "index.html",
+    });
+
+    // --- Outputs ---
     new cdk.CfnOutput(this, "FrontendURL", {
       value: siteBucket.bucketWebsiteUrl,
     });
 
     new cdk.CfnOutput(this, "ApiEndpoint", {
       value: api.apiEndpoint,
+    });
+
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      value: "https://" + distribution.distributionDomainName,
     });
   }
 }
